87a19201ddd1c8f64b0664a691ec8584fd3de195
bccarruth927 <brecarr1997@aol.com>
2023-07-21 15:09:59 -0400

    Renamed, reorganized, relinked JS files

--- js/animations.js
+++ js/animations.js
@@ -1,31 +1,4 @@
-//Main JavaScript file
-//Import the validation function from form-validation
-
 $(document).ready(() => {
-    //Mobile menu toggle functionality
-    $('.toggle').on('click', () => {
-        $('.item').slideToggle();
-    });
-
-    /*
-    const toggle = document.querySelector('.toggle');
-    const menu = document.querySelector('.menu');
-
-    const toggleMenu = () => {
-        if (menu.classList.contains('active')) {
-            menu.classList.remove('active');
-
-            toggle.querySelector('a').innerHTML = '<i class="fas fa-bars"></i>';
-        } else {
-            menu.classList.add('active');
-
-            toggle.querySelector('a').innerHTML = '<i class="fas fa-times"></i>'
-        }
-    };
-
-    toggle.addEventListener('click', toggleMenu, false);
-    */
-    
     //Scroll animation for the mobile Projects layout
     $('#mobile-project1-container').waypoint(() => {
         $('#mobile-project1-container').addClass('animate__animated animate__fadeInUp');
---
+++ js/mobile-toggle.js
@@ -0,0 +1,28 @@
+//Main JavaScript file
+//Import the validation function from form-validation
+
+$(document).ready(() => {
+    //Mobile menu toggle functionality
+    $('.toggle').on('click', () => {
+        $('.item').slideToggle();
+    });
+
+    /*
+    const toggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+
+    const toggleMenu = () => {
+        if (menu.classList.contains('active')) {
+            menu.classList.remove('active');
+
+            toggle.querySelector('a').innerHTML = '<i class="fas fa-bars"></i>';
+        } else {
+            menu.classList.add('active');
+
+            toggle.querySelector('a').innerHTML = '<i class="fas fa-times"></i>'
+        }
+    };
+
+    toggle.addEventListener('click', toggleMenu, false);
+    */
+});
